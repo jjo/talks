2839401bb7cf44239bb95f212a3484bb16f07f49
JuanJo Ciarlante <juanjosec@gmail.com>
2018-10-23 14:57:15 +0200

    add PUSHGATEWAY_URL envvar

--- 2018/metricfy-the-world/metrics-pusher.sh
+++ 2018/metricfy-the-world/metrics-pusher.sh
@@ -6,9 +6,7 @@ get_builds_counter() {
         bar) idx=11;;
     esac
     # Fake some monotonically increasing number
-    set -x
-    awk "/eth0:/{ print \$$idx }" /proc/net/dev
-    set +x
+    awk "/(eth0|wlp3s0):/{ print \$$idx ; exit 0}" /proc/net/dev
 }
 generate_metrics() {
 cat << EOF
@@ -20,7 +18,11 @@ EOF
 }
 JOB=jenkins
 INSTANCE=app
-while sleep 0.1; do
-    generate_metrics | curl -s --data-binary @- http://pushgateway:9091/metrics/job/$JOB/instance/$INSTANCE
-    sleep 1.9 || break
-done
+if [ -n "$PUSHGATEWAY_URL" ]; then
+    while sleep 0.1; do
+        generate_metrics | curl -s --data-binary @- $PUSHGATEWAY_URL/metrics/job/$JOB/instance/$INSTANCE
+        sleep 1.9 || break
+    done
+else
+    generate_metrics
+fi
