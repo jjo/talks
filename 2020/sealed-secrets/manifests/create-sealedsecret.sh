0ee0fe53afd82a48b558cbac8b3bd8d5facb07cc
JuanJo Ciarlante <juanjosec@gmail.com>
2020-05-21 13:32:21 -0300

    sealed-secrets example: s/db-password/db_password/ for easier jq'ing

--- 2020/sealed-secrets/manifests/create-sealedsecret.sh
+++ 2020/sealed-secrets/manifests/create-sealedsecret.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -v
-kubectl create secret generic mydb-secret --from-literal=db-password=$(read -s -p "pass: " pass; echo $pass) -oyaml --dry-run=client |\
-   kubeseal > mydb.sealedsecret.yaml
+kubectl create secret generic mydb-secret --from-literal=db_password=$(read -s -p "pass: " pass; echo $pass) -oyaml --dry-run=client |\
+   kubeseal "${@}" > mydb.sealedsecret.yaml
