a512d11afdb8e7ee69fe106e2fcdcfe37d64d95f
JuanJo Ciarlante <juanjosec@gmail.com>
2020-05-21 12:11:49 -0300

    minors

--- 2020/sealed-secrets/manifests/create-sealedsecret.sh
+++ 2020/sealed-secrets/manifests/create-sealedsecret.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-kubectl create secret generic mydb-secret --from-literal=db-password=$(read -s -p "pass: " pass; echo $^Css) -oyaml --dry-run=client |\
+set -v
+kubectl create secret generic mydb-secret --from-literal=db-password=$(read -s -p "pass: " pass; echo $pass) -oyaml --dry-run=client |\
    kubeseal > mydb.sealedsecret.yaml
